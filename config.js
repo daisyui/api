564e2ad4ccfaff118235fd41fe4ca2a84903207d
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2024-09-16 19:55:05 +0300

    .

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ export const config = {
     { value: 5 * 60, chance: 20 },
     { value: 6 * 60, chance: 10 },
   ],
-  logFiles: ["./source/142623.js", "./source/144550.js"],
+  logFiles: ["./source/142623.js", "./source/144550.js", "./source/351127.js"],
   serverId: "1204154732978118716",
   channelId: "1204197126775504926",
   storeId: "10640",
