d582893fee5f1b01fdf6fa030f30b252b8b5797f
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2025-04-04 20:34:59 +0300

    .

--- source/144550.js
+++ source/144550.js
@@ -1,6 +1,6 @@
 import { url, params, template } from "./default/orders.js";
 export { url, params, template };
-const products = [144550, 208203, 284327, 426780];
+const products = [144550, 208203, 284327, 426780, 479479];
 export const filter = (order) => {
   return products.includes(order?.attributes?.first_order_item?.product_id);
 };
